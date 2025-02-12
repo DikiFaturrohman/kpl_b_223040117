171583aaceecacbbc76239e89ee586fa8d193d9d
IniFadhil <rffadhil07@gmail.com>
2025-02-12 13:53:51 +0700

    Mengerjakan Card Title

--- ditel.php
+++ ditel.php
@@ -2,27 +2,24 @@
 require 'functions.php';
 $id = $_GET["id"];
 $rows = query("SELECT * FROM halaman WHERE id = $id");
-
 ?>
 
-<div class="row mt-4 internasional-populer">
-    <div class="col">
-        <!-- Card -->
-        <div class="container">
-            <div class="row">
-                <?php $count = 0; ?>
-                <?php foreach ($rows as $row) : ?>
-                    <div class="card mx-3 d-inline-block" style="width: 18rem;">
-                        <img src="img/<?= $row['gambar']; ?>" class="card-img-top" alt="" />
-                        <div class="card-body">
-                            <h1 class="card-title"><?= $row['judul']; ?></h1>
-                            <p class="card-text"><?= $row['kutipan']; ?></p>
-                            <p class="card-text"><?= $row['isi']; ?></p>
-                        </div>
-                    </div>
-                    <?php $count++; ?>
-                <?php endforeach; ?>
-                <button class="btn btn-danger" onclick="window.print()">download</button>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <?php foreach ($rows as $row) : ?>
+            <div class="card h-100 shadow-sm">
+                <img src="img/<?= $row['gambar']; ?>" class="card-img-top img-fluid rounded" alt="Gambar Artikel">
+                <div class="card-body">
+                    <h1 class="card-title text-center"><?= $row['judul']; ?></h1>
+                    <hr>
+                    <p class="card-text"><strong>Kutipan:</strong> <?= $row['kutipan']; ?></p>
+                    <p class="card-text"><?= nl2br($row['isi']); ?></p>
+                </div>
+            </div>
+            <?php endforeach; ?>
+            <div class="text-center mt-4">
+                <button class="btn btn-danger" onclick="window.print()">Download</button>
             </div>
         </div>
     </div>
